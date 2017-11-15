04602f6f25d9f2c70841ce26558862653e606f51
Tristan de Boer <info@tristandeboer.nl>
2017-11-15 15:54:43 +0100

    Split up MNIST dataset into training, test and validation set.

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -10,3 +10,11 @@ from mnist import read, show
 train = list(read('train'))
 test = list(read('test'))
 
+# Split up MNIST dataset into training, test and validation set
+np.random.shuffle(test)
+test_split = int(0.9 * len(test))
+test, validation = test[:test_split], test[test_split:]
+
+print("Train size: {}".format(len(train)))
+print("Test size: {}".format(len(test)))
+print("Validation size: {}".format(len(validation)))
