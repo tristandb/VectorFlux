1107105115ec4232a79542bbab11abed547ed856
Tristan de Boer <info@tristandeboer.nl>
2017-11-15 15:44:22 +0100

    Initial commit

---
+++ neuralnetwork.py
@@ -0,0 +1,12 @@
+"""
+Implements a Neural Network
+
+"""
+
+import numpy as np
+from mnist import read, show
+
+# Import training and test set
+train = list(read('train'))
+test = list(read('test'))
+
