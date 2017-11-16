d2834e472a821d5996dc98a0afd084f101ec76f3
Tristan de Boer <info@tristandeboer.nl>
2017-11-16 11:00:48 +0100

    Added initial Neural Network

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,23 +1,34 @@
-"""
-Implements a Neural Network
-
-"""
-
+from numpy import exp, array, random, dot
 import numpy as np
-from mnist import read, show, normalize
+import math
 
-# Import training and test set
-train = list(read('train'))
-test = list(read('test'))
+class NeuralNetwork():
+    def __init__(self, input_size):
+        random.seed(1)
+        self.synaptic_weights = 2 * random.random((input_size, 1)) - 1
 
-# Split up MNIST dataset into training, test and validation set
-np.random.shuffle(test)
-test_split = int(0.9 * len(test))
-test, validation = test[:test_split], test[test_split:]
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
 
-print("Train size: {}".format(len(train)))
-print("Test size: {}".format(len(test)))
-print("Validation size: {}".format(len(validation)))
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
 
-# Normalization for values
-validation = normalize(validation)
+    def train(self, training_set_inputs, training_set_outputs, iterations, test_set_inputs, test_set_outputs, test_interval):
+        for iteration in range(iterations):
+            if (iteration % test_interval == 0):
+                print("Iteration {} of {}".format(iteration, iterations))
+                self.test(test_set_inputs, test_set_outputs)
+            output = self.think(training_set_inputs)
+
+            error = (training_set_outputs - output) ** 2
+
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            self.synaptic_weights += adjustment
+
+    def test(self, test_set_inputs, test_set_outputs):
+        output = self.think(test_set_inputs)
+        print(np.mean((output - test_set_outputs) ** 2))
+
+    def think(self, inputs):
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
